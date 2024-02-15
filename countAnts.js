5750bae1eba9aa3c7106ec3fd5ea1982fcbf9081
Pedro Javier Lora <javierpd45@gmail.com>
2024-02-15 16:54:22 -0400

    Ant counter in JavaScript, using nodejs

---
+++ countAnts.js
@@ -0,0 +1,18 @@
+let deadAnts = '';
+
+const antChain = '...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t';
+
+deadAntCounter = function(antChain) 
+{
+        deadAnts = antChain.replaceAll('ant', '');
+
+        let heads = deadAnts.match(/a/g).length;
+        let bodys = deadAnts.match(/n/g).length;
+        let tails = deadAnts.match(/t/g).length;
+
+        let totalDeadAnts = Math.max(heads, bodys, tails);
+        return totalDeadAnts;
+}
+
+let totalDead = deadAntCounter(antChain);
+console.log("The amount of dead ants is: " + totalDead);
